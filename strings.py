95ce4e59ed9b3d78e5c02076fa89ac77bc3fa089
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-16 17:02:36 +0900

    string

---
+++ strings.py
@@ -0,0 +1,12 @@
+course = 'Python for Beginners'
+print(course[0]) # P
+print(course[-1]) # s
+print(course[-2]) # r
+print(course[0:3]) # Pyt
+print(course[0:]) # Python for Beginners
+print(course[1:]) # ython for Beginners
+print(course[:5]) # Pytho
+print(course[:]) # Python for Beginners
+
+name="Jennifer"
+print(name[1:-1]) # ennife
