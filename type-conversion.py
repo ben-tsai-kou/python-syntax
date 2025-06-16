cf3d7eeb22847a9f7e76c3cfee26a935b6d091ae
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-16 16:40:09 +0900

    type conversion

---
+++ type-conversion.py
@@ -0,0 +1,8 @@
+# birth_year = input('Birth year: ')
+# age = 2025 - int(birth_year)
+# print(age)
+
+
+question = input('what is your weight in kilograms?')
+pound = float(question) * 2.205
+print('your weight convert to lbs is ' + str(pound))
