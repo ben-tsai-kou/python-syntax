c79dee3dbcadbcf301d8bb56183190b7f08ba23c
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-17 15:17:09 +0900

    logical operators

---
+++ logical-operators.py
@@ -0,0 +1,11 @@
+# has_high_income = True
+# has_good_credit = False
+#
+# if has_good_credit and has_high_income:
+#     print('Eligible for load')
+
+has_good_credit = True
+has_criminal_record = False
+
+if has_good_credit and not has_criminal_record:
+    print('Eligible for load')
