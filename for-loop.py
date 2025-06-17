902d4dd4909cdde9a0ceae29f3ff664e146eb644
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-17 16:29:53 +0900

    for loops

---
+++ for-loop.py
@@ -0,0 +1,16 @@
+for item in 'Python':
+    print(item)
+
+for item in ['Mosh', 'John', 'Sarah']:
+    print(item)
+
+for item in range(10): # 0 ~ 9
+    print(item)
+
+for item in range(5, 10): # 5 ~ 9
+    print(item)
+
+for item in range(5, 10, 2): # 5, 7, 9
+    print(item)
+
+
