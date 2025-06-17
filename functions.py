7bc4256517786447f545478a6ef379f0c414ef1d
ben-tsai-kou <97225493+ben-tsai-kou@users.noreply.github.com>
2025-06-17 18:35:15 +0900

    keyword arguments

---
+++ functions.py
@@ -0,0 +1,8 @@
+def greet_user(first_name, last_name):
+    print(f'hi {first_name} {last_name}!')
+    print('welcome aboard!')
+
+
+print('Start')
+greet_user(last_name='Tsai', first_name='Ben')
+print('Finish')
